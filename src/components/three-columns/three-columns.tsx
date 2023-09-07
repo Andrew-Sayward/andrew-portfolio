fb29cdc101f612b97f8b8d1840f9bc046c4eade0
Andrew Sayward <andrew.sayward@wearewiser.com>
2023-09-07 10:33:37 +0100

    add 3 columns section

--- src/components/three-columns/three-columns.tsx
+++ src/components/three-columns/three-columns.tsx
@@ -1,17 +1,68 @@
 "use client";
 
-import { useEffect } from "react";
+import { MutableRefObject, useEffect, useRef } from "react";
 import styles from "./three-columns.module.scss";
 import { Bangers } from "next/font/google";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/all";
 
 const bangers = Bangers({ subsets: ["latin"], weight: "400" });
+gsap.registerPlugin(ScrollTrigger); // Register ScrollTrigger
 
 const ThreeColumns = () => {
+  const containerRef = useRef<HTMLDivElement>() as MutableRefObject<HTMLDivElement>;
+  const tl = useRef<gsap.core.Timeline>();
+
+  useEffect(() => {
+    const container = containerRef.current;
+
+    let ctx = gsap.context(() => {
+      tl.current = gsap.timeline({
+        scrollTrigger: {
+          trigger: container,
+          scrub: true,
+          start: "top bottom",
+          end: "bottom bottom",
+        },
+      });
+      if (window.innerWidth > 910) {
+        tl.current.from(
+          container.querySelectorAll(".column-item"),
+          {
+            y: "70vh",
+            stagger: {
+              each: 0.2,
+            },
+          },
+          "-=1"
+        );
+      } else {
+        tl.current.from(
+          container.querySelectorAll(".column-item"),
+          {
+            x: "100vw",
+            stagger: {
+              each: 0.3,
+            },
+          },
+          "-=1"
+        );
+      }
+    });
+
+    return () => {
+      ctx.revert();
+    };
+  }, []);
+
   return (
-    <section className={`${styles.threeColumns} min-h-screen`}>
-      <div className={styles.column1}></div>
-      <div className={styles.column2}></div>
-      <div className={styles.column3}></div>
+    <section className={`${styles.threeColumns} min-h-screen overflow-hidden`} ref={containerRef}>
+      <div className={styles.columns}>
+        <h2>I work with</h2>
+        <div className={`column-item ${styles.column1}`}>React / ES6</div>
+        <div className={`column-item ${styles.column2}`}>HTML / JSX</div>
+        <div className={`column-item ${styles.column3}`}>SCSS & More</div>
+      </div>
     </section>
   );
 };
