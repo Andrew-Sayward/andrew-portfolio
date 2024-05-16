7b697e7b921faa431ad1b30652e20574296ac940
Andrew Sayward <andrew.sayward@wearewiser.com>
2024-05-16 15:13:59 +0100

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,46 +9,6 @@ import { useEffect } from "react";
 function MyApp({ Component, pageProps }: any) {
   const router = useRouter();
 
-  useEffect(() => {
-    const handleRouteChange = () => {
-      requestAnimationFrame(() => {
-        // Clone only essential style nodes to reduce overhead
-        const nodes = document.querySelectorAll("link[rel=stylesheet], style");
-        const copies = Array.from(nodes).map((el) => el.cloneNode(true));
-
-        copies.forEach((copy) => {
-          if (copy instanceof Element) {
-            // Ensure the node is an Element
-            copy.removeAttribute("data-n-p");
-            copy.removeAttribute("data-n-href");
-            document.head.appendChild(copy);
-          }
-        });
-
-        // Cleanup function
-        const cleanup = () => {
-          requestAnimationFrame(() => {
-            copies.forEach((copy) => {
-              if (copy instanceof Element) {
-                // Ensure the node is an Element
-                document.head.removeChild(copy);
-              }
-            });
-          });
-        };
-
-        // Setup cleanup after animation completes
-        // Assuming a fixed transition duration, adjust as needed
-        setTimeout(cleanup, 1000); // Adjust this timeout to your transition duration
-      });
-    };
-
-    router.events.on("routeChangeStart", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeStart", handleRouteChange);
-    };
-  }, []); // Empty dependency array to run only once
-
   return (
     <>
       <Head>
